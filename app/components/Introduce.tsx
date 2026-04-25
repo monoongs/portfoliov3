79e44c4f60d5002d8177355ceb03353fe090080e
Casper <aong@Nattapons-MacBook-Pro-M3.local>
2026-04-25 21:29:03 +0700

    feat: adjust home

--- app/components/Introduce.tsx
+++ app/components/Introduce.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image';
-import CommonContainer from './CommonContainer';
+import { differenceInYears } from 'date-fns';
 
 function Introduce() {
+  const today = new Date();
+  const startWorkDate = new Date(2018, 8);
+
+  const experience = differenceInYears(today, startWorkDate);
   return (
     <>
       <div className='relative w-48 h-48 rounded-full overflow-hidden self-center'>
@@ -19,8 +23,16 @@ function Introduce() {
 
       <div className='flex items-center justify-center'>
         <p className='text-left font-medium uppercase'>
-          Software Engineer building scalable web applications using modern tech
-          stack
+          Software Engineer with {experience}+ years of experience building
+          scalable web applications using modern tech stack
+        </p>
+      </div>
+
+      <div className='flex items-center justify-center pt-5'>
+        <p className='text-left font-medium uppercase italic'>
+          <span className='font-light underline'>Nest.js</span> |{' '}
+          <span className='font-light underline'>Next.js</span> |{' '}
+          <span className='font-light underline'>TypeScript</span>
         </p>
       </div>
     </>
