da688c0a8e829d28dae70b0bd935c8e912700e3f
Nattapon.srivi <monoongs@gmail.com>
2026-04-27 16:48:35 +0700

    feat: adjust name

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/app/components/Navbar';
+import { profile } from '@/app/config';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -14,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'Nattapon Srivithayarat Portfolio',
+  title: `${profile.name} Portfolio`,
   description:
     'Software Engineer building scalable web applications using modern tech stack',
 };
