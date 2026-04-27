0582f3438c617217bb09df2fd303e611fb1ca2d6
Nattapon.srivi <monoongs@gmail.com>
2026-04-27 14:20:31 +0700

    feat: add Link to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,10 +1,13 @@
 import ProfileDialog from '@/app/components/ProfileDialog';
+import Link from 'next/link';
 
 function Navbar() {
   return (
     <div className='flex justify-between items-center px-8 py-5 fixed top-0 left-0 right-0 z-50 bg-white'>
       <div>
-        <h1 className='text-md font-bold'>NATTAPON.DEV</h1>
+        <Link href='/' className='hover:underline'>
+          <h1 className='text-md font-bold'>NATTAPON.DEV</h1>
+        </Link>
       </div>
       <div>
         <ProfileDialog />
