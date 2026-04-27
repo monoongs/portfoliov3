6c6e481a6a055c7330308e7e85bae1c86b0ea872
Nattapon.srivi <monoongs@gmail.com>
2026-04-27 16:22:11 +0700

    fix: shadow on navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 function Navbar() {
   return (
-    <div className='fixed top-0 left-0 right-0 z-50 bg-gray-100'>
-      <div className='max-w-3xl mx-auto px-10 flex justify-between items-center py-5 bg-white shadow-[20px_0_25px_-5px_rgb(0_0_0/0.1),-20px_0_25px_-5px_rgb(0_0_0/0.1)] [clip-path:inset(0_-30px)]'>
+    <div className='fixed top-0 left-0 right-0 z-50'>
+      <div className='max-w-3xl mx-auto px-10 flex justify-between items-center py-5 bg-white'>
         <div>
           <Link href='/' className='hover:underline'>
             <h1 className='text-md font-bold'>NATTAPON.DEV</h1>
