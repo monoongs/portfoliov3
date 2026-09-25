40e5f222ae72ada7fec73ffec7d6cd3764deb65b
Nattapon.srivi <monoongs@gmail.com>
2026-09-26 00:15:20 +0700

    feat: wrap button

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,65 +1,73 @@
+'use client';
+
 import { FaLinkedin, FaGithub } from 'react-icons/fa';
 import { FiDownload } from 'react-icons/fi';
 import Link from 'next/link';
 import { profile } from '@/app/config';
 import { Button } from '@/components/ui/button';
+import { track } from '@vercel/analytics';
+import { ANALYTIC_TARGET } from '@/app/definations/analytic';
 
 function Footer() {
   return (
-    <div className='bg-[#ececed] -mx-10 px-10 py-5'>
-      <div className='flex flex-col'>
-        <h4 className='text-xl font-bold uppercase'>Want to work together ?</h4>
-        <p className='text-xl font-bold uppercase'>Let&apos;s talk at</p>
+    <div className="bg-[#ececed] -mx-10 px-10 py-5">
+      <div className="flex flex-col">
+        <h4 className="text-xl font-bold uppercase">Want to work together ?</h4>
+        <p className="text-xl font-bold uppercase">Let&apos;s talk at</p>
         <Link
           href={`mailto:${profile.email}`}
-          className='text-xl font-semibold underline uppercase mt-2'
+          className="text-xl font-semibold underline uppercase mt-2"
+          onClick={() => track(ANALYTIC_TARGET.EMAIL_CLICK)}
         >
           {profile.email}
         </Link>
-        <div className='flex gap-4 mt-5 items-center'>
-          <Button variant='outline' className='rounded-full text-xs font-bold'>
+        <div className="flex gap-4 mt-5 items-center">
+          <Button variant="outline" className="rounded-full text-xs font-bold">
             <Link
               href={profile.cvUrl}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='flex items-center gap-2'
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center gap-2"
+              onClick={() => track(ANALYTIC_TARGET.CV_DOWNLOAD)}
             >
               <FiDownload size={16} /> GET MY CV
             </Link>
           </Button>
 
-          <div className='w-px h-6 bg-gray-500' />
+          <div className="w-px h-6 bg-gray-500" />
 
           <Button
-            variant='outline'
-            className='rounded-full text-xs font-bold'
+            variant="outline"
+            className="rounded-full text-xs font-bold"
             asChild
           >
             <Link
               href={profile.linkedin}
-              target='_blank'
-              rel='noopener noreferrer'
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={() => track(ANALYTIC_TARGET.LINKEDIN_CLICK)}
             >
-              <FaLinkedin size={16} className='text-black' />
+              <FaLinkedin size={16} className="text-black" />
             </Link>
           </Button>
 
           <Button
-            variant='outline'
-            className='rounded-full text-xs font-bold'
+            variant="outline"
+            className="rounded-full text-xs font-bold"
             asChild
           >
             <Link
               href={profile.github}
-              target='_blank'
-              rel='noopener noreferrer'
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={() => track(ANALYTIC_TARGET.GITHUB_CLICK)}
             >
-              <FaGithub size={16} className='text-black' />
+              <FaGithub size={16} className="text-black" />
             </Link>
           </Button>
         </div>
 
-        <h5 className='text-4xl uppercase font-bold mt-5'>{profile.name}</h5>
+        <h5 className="text-4xl uppercase font-bold mt-5">{profile.name}</h5>
       </div>
     </div>
   );
