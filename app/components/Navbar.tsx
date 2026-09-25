81ec1c99093d9189d31d7cfcf2f59043ee58ba45
Nattapon.srivi <monoongs@gmail.com>
2026-09-26 00:19:30 +0700

    feat: add smooth scroll

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,7 +13,16 @@ function Navbar() {
     <div className="fixed top-0 left-0 right-0 z-50">
       <div className="max-w-3xl mx-auto px-10 flex justify-between items-center py-5 bg-white">
         <div>
-          <Link href="/" className="hover:underline">
+          <Link
+            href="/"
+            className="hover:underline"
+            onClick={(event) => {
+              if (window.location.pathname !== '/') return;
+
+              event.preventDefault();
+              window.scrollTo({ top: 0, behavior: 'smooth' });
+            }}
+          >
             <h1 className="text-md font-bold">NATTAPON.DEV</h1>
           </Link>
         </div>
